d28c06858bc1934da9b721446dafd76a8b90cc72
Usevalad Buben <usevaladbuben@gmail.com>
2025-04-16 17:16:52 +0300

    Complete basket generation

---
+++ Usevalad Fruit Blast/Assets/_Project/Scripts/Repositories/Context.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Repositories
+{
+    public class Context
+    {
+        public static readonly GameObject Container = new();
+    }
+}
---
+++ Usevalad Fruit Blast/Assets/_Project/Scripts/System/Installers/BasketInstaller.cs
@@ -0,0 +1,18 @@
+﻿using _Project.Scripts.Features.Basket.BasketSpawning;
+using _Project.Scripts.Utils;
+using UnityEngine;
+
+namespace _Project.Scripts.System.Installers
+{
+    public class BasketInstaller: MonoBehaviour, ISystemInstaller
+    {
+        [SerializeField] private Vector2Int size;
+        [SerializeField] private Margin margin;
+        
+        public void Install()
+        {
+            IBasketSpawner basketSpawner = new BasketSpawner();
+            basketSpawner.SpawnBasket(size, margin);
+        }
+    }
+}
