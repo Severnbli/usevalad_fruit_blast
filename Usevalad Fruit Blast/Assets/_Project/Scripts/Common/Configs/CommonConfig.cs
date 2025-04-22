f558adf4cdf28b9931b110cab2257645efa1946b
Usevalad Buben <usevaladbuben@gmail.com>
2025-04-22 22:30:50 +0300

    Setup system configurator

--- Usevalad Fruit Blast/Assets/_Project/Scripts/Common/Configs/CommonConfig.cs
+++ Usevalad Fruit Blast/Assets/_Project/Scripts/Common/Configs/CommonConfig.cs
@@ -1,23 +1,31 @@
-﻿using System;
-using _Project.Scripts.Features.Field.FieldCatcher.ColliderFieldCatcher;
-using _Project.Scripts.Features.Field.FieldProvider.CameraFieldProvider;
+﻿using _Project.Scripts.Features.Field.FieldCatcher;
+using _Project.Scripts.Features.Field.FieldProvider;
 using _Project.Scripts.Features.Gizmo.GizmoProvider.BaseGizmoProvider;
+using _Project.Scripts.Features.Physics.Engine.Config;
 using _Project.Scripts.Features.Physics.Forces.GravityForce;
+using _Project.Scripts.Features.Random.Config;
+using _Project.Scripts.Features.Spawners.BallSpawner;
 using UnityEngine;
 
 namespace _Project.Scripts.Common.Configs
 {
-    [Serializable]
     public class CommonConfig : MonoBehaviour
     {
-        [SerializeField] private CameraFieldProviderConfig _cameraFieldProviderConfig;
-        [SerializeField] private ColliderFieldCatcherConfig _colliderFieldCatcherConfig;
-        [SerializeField] private BaseGizmoProviderConfig _baseGizmoProviderConfig;
+        [SerializeField] private PhysicsEngineConfig _physicsEngineConfig;
         [SerializeField] private GravityForceProviderConfig _gravityForceProviderConfig;
+        [SerializeField] private BaseGizmoProviderConfig _baseGizmoProviderConfig;
+        [SerializeField] private RandomProviderConfig _randomProviderConfig;
         
-        public CameraFieldProviderConfig CameraFieldProviderConfig => _cameraFieldProviderConfig;
-        public ColliderFieldCatcherConfig ColliderFieldCatcherConfig => _colliderFieldCatcherConfig;
+        [SerializeField] private FieldCatcher _fieldCatcher;
+        [SerializeField] private FieldProvider _fieldProvider;
+        [SerializeField] private BallSpawner _ballSpawner;
+        
+        public PhysicsEngineConfig PhysicsEngineConfig => _physicsEngineConfig;
         public BaseGizmoProviderConfig BaseGizmoProviderConfig => _baseGizmoProviderConfig;
         public GravityForceProviderConfig GravityForceProviderConfig => _gravityForceProviderConfig;
+        public RandomProviderConfig RandomProviderConfig => _randomProviderConfig;
+        public FieldCatcher FieldCatcher => _fieldCatcher;
+        public FieldProvider FieldProvider => _fieldProvider;
+        public BallSpawner BallSpawner => _ballSpawner;
     }
 }
