7681c3859e54f1ab72bd152f6d78caeea042f3ea
Usevalad Buben <usevaladbuben@gmail.com>
2025-04-25 01:12:29 +0300

    Add touch object destroyer feature

---
+++ Usevalad Fruit Blast/Assets/_Project/Scripts/Features/Touch/TouchHandler/TouchObjectDestroyer/TouchObjectDestroyer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Features.Touch.TouchHandler.TouchObjectDestroyer
+{
+    public class TouchObjectDestroyer : TouchHandler
+    {
+        public override void ProcessTouch(Vector2 position)
+        {
+            ;
+        }
+    }
+}
