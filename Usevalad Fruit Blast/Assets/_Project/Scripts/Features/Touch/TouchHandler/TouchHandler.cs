4773ef29b20fd6902776e0c0b905256e7197e3d9
Usevalad Buben <usevaladbuben@gmail.com>
2025-04-25 01:12:06 +0300

    Add touch handler feature

---
+++ Usevalad Fruit Blast/Assets/_Project/Scripts/Features/Touch/TouchHandler/TouchHandler.cs
@@ -0,0 +1,39 @@
+﻿using _Project.Scripts.Features.Common;
+using _Project.Scripts.Features.Field.FieldProvider;
+using UnityEngine;
+
+namespace _Project.Scripts.Features.Touch.TouchHandler
+{
+    public abstract class TouchHandler : BaseFeature
+    {
+        [SerializeField] protected FieldProvider _fieldProvider;
+        
+        public FieldProvider FieldProvider => _fieldProvider;
+        
+        public void Update()
+        {
+            CheckTouch();
+        }
+
+        private void CheckTouch()
+        {
+            if (Input.touchCount <= 0)
+            {
+                return;
+            }
+
+            var touch = Input.GetTouch(0);
+
+            if (touch.phase != TouchPhase.Began)
+            {
+                return;
+            }
+            
+            ProcessTouch(_fieldProvider.GetConvertedScreenSpacePosition(touch.position));
+        }
+
+        public abstract void ProcessTouch(Vector2 position);
+        
+        public override void Init(IFeatureConfig config) {}
+    }
+}
