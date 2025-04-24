45815218407d0d2451d16d255d1d1e8b5cc87f0e
Usevalad Buben <usevaladbuben@gmail.com>
2025-04-24 13:39:44 +0300

    Add spawned objects configs

--- Usevalad Fruit Blast/Assets/_Project/Scripts/Features/Spawners/ObjectSpawner.cs
+++ Usevalad Fruit Blast/Assets/_Project/Scripts/Features/Spawners/ObjectSpawner.cs
@@ -1,15 +1,26 @@
-﻿using System;
-using _Project.Scripts.Features.Common;
+﻿using _Project.Scripts.Features.Common;
+using _Project.Scripts.Features.Random;
+using _Project.Scripts.System;
 using UnityEngine;
 
 namespace _Project.Scripts.Features.Spawners
 {
     public abstract class ObjectSpawner : BaseFeature
     {
-        [SerializeField] protected GameObject _prefab;
+        protected RandomProvider _randomProvider;
         
-        public GameObject Prefab => _prefab;
+        public RandomProvider RandomProvider => _randomProvider;
         
         public abstract void Spawn();
+        
+        public override void Init(IFeatureConfig config)
+        {
+            _randomProvider = Context.Container.GetComponent<RandomProvider>();
+
+            if (_randomProvider == null)
+            {
+                Debug.LogError("Check system priority setup: random provider must be earlier than object spawner!");
+            }
+        }
     }
 }
