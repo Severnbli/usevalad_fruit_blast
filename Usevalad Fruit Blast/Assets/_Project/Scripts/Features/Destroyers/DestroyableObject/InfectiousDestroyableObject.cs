58f8847f27e7b02c129e198d8fe830670e1e64c1
Usevalad Buben <usevaladbuben@gmail.com>
2025-04-25 10:21:15 +0300

    Add infectious destroyable object and it processing

---
+++ Usevalad Fruit Blast/Assets/_Project/Scripts/Features/Destroyers/DestroyableObject/InfectiousDestroyableObject.cs
@@ -0,0 +1,48 @@
+﻿using _Project.Scripts.System;
+using UnityEngine;
+
+namespace _Project.Scripts.Features.Destroyers.DestroyableObject
+{
+    public class InfectiousDestroyableObject : DestroyableObject
+    {
+        [SerializeField] private int _infectiousGroupId;
+        [SerializeField] private float _infectiousRadius;
+        
+        public int InfectiousGroupId => _infectiousGroupId;
+        public float InfectiousRadius => _infectiousRadius;
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            
+            DestroySameInfectiousGroupByInfectiousRadius();
+        }
+
+        private void DestroySameInfectiousGroupByInfectiousRadius()
+        {
+            if (!Context.TryGetComponentsFromContainer<ObjectDestroyer>(out var destroyers))
+            {
+                return;
+            }
+
+            foreach (var destroyer in destroyers)
+            {
+                foreach (var destroyableObject in destroyer.DestroyableObjects)
+                {
+                    if (destroyableObject is not InfectiousDestroyableObject infectiousDestroyableObject
+                        || infectiousDestroyableObject.InfectiousGroupId != _infectiousGroupId)
+                    {
+                        continue;
+                    }
+                    
+                    var distance = Vector2.Distance(destroyableObject.transform.position, transform.position);
+
+                    if (distance <= _infectiousRadius)
+                    {
+                        Destroy(destroyableObject.gameObject);
+                    }
+                }
+            }
+        }
+    }
+}
