ebe707e6e1abed77fdf8b980b5c2331ddc438df0
Usevalad Buben <usevaladbuben@gmail.com>
2025-05-04 02:05:07 +0300

    Remove FieldProvider field from ScaleProvider class

--- Usevalad Fruit Blast/Assets/_Project/Scripts/Features/Dimensions/Scale/ScaleProvider/ScaleProvider.cs
+++ Usevalad Fruit Blast/Assets/_Project/Scripts/Features/Dimensions/Scale/ScaleProvider/ScaleProvider.cs
@@ -9,26 +9,11 @@ namespace _Project.Scripts.Features.Dimensions.Scale.ScaleProvider
     public class ScaleProvider : BaseFeature, IConfigurableFeature<ScaleProviderConfig>
     {
         [SerializeField] private ScaleProviderConfig _scaleProviderConfig;
-        [SerializeField] private FieldProvider _fieldProvider;
         
         public ScaleProviderConfig ScaleProviderConfig => _scaleProviderConfig;
-        public FieldProvider FieldProvider => _fieldProvider;
         
         public float Scale { get; protected set; }
         public event Action<float> OnChangeScale;
-        
-        public override void Init()
-        {
-            base.Init();
-
-            if (!Context.TryGetComponentFromContainer(out FieldProvider fieldProvider))
-            {
-                Debug.LogError("Check system priority setup: field provider must be earlier than scale provider!");
-                return;
-            }
-            
-            _fieldProvider = fieldProvider;
-        }
 
         public void Configure(ScaleProviderConfig scaleProviderConfig)
         {
