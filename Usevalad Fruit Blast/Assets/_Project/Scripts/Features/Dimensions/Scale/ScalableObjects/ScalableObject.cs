86c5603a5bcaadf4748715b6c1b1cfc159924ebe
Usevalad Buben <usevaladbuben@gmail.com>
2025-05-04 02:30:49 +0300

    Improve ScaleProvider & ScalableObject
    ScaleProvider:
    - Add field to store last scale
    - Add FixedUpdate function that view on screen resolution changes
    - Add function ChangeScale and prepare event OnChangeScale
    
    ScalableObject:
    - Add functionality to connect to and disconnect from ScaleProvider (subscribe / unsubscribe to event)
    - Add field that store original scale (that helps rightly update scale on multiply iterations)
    
    Ball.prefab
    - Add IdentifableObject and ScalableObject on it as components

--- Usevalad Fruit Blast/Assets/_Project/Scripts/Features/Dimensions/Scale/ScalableObjects/ScalableObject.cs
+++ Usevalad Fruit Blast/Assets/_Project/Scripts/Features/Dimensions/Scale/ScalableObjects/ScalableObject.cs
@@ -1,9 +1,64 @@
-﻿using UnityEngine;
+﻿using _Project.Scripts.System;
+using UnityEngine;
 
 namespace _Project.Scripts.Features.Dimensions.Scale.ScalableObjects
 {
     public class ScalableObject : MonoBehaviour
     {
+        private Vector3 _baseScale;
         
+        private void Start()
+        {
+            SetBaseScale(transform.localScale);
+            ConnectToScaleProvider();
+        }
+        
+        private void OnDestroy()
+        {
+            DisconnectFromScaleProvider();
+        }
+
+        private void SetBaseScale(Vector3 baseScale)
+        {
+            _baseScale = baseScale;
+        }
+
+        private void ConnectToScaleProvider()
+        {
+            if (!TryGetScaleProvider(out var scaleProvider))
+            {
+                return;
+            }
+            
+            scaleProvider.OnChangeScale += UpdateScale;
+        }
+
+        private void DisconnectFromScaleProvider()
+        {
+            if (!TryGetScaleProvider(out var scaleProvider))
+            {
+                return;
+            }
+            
+            scaleProvider.OnChangeScale -= UpdateScale;
+        }
+
+        private bool TryGetScaleProvider(out ScaleProvider.ScaleProvider scaleProvider)
+        {
+            scaleProvider = null;
+            
+            if (Context.TryGetComponentFromContainer(out scaleProvider))
+            {
+                return true;
+            }
+            
+            Debug.LogError("Check system priority setup: scale provider must be earlier than scalable object!");
+            return false;
+        }
+
+        private void UpdateScale(float scale)
+        {
+            transform.localScale = _baseScale * scale;
+        }
     }
 }
--- Usevalad Fruit Blast/Assets/_Project/Scripts/Features/Dimensions/Scale/ScaleProvider/ScaleProvider.cs
+++ Usevalad Fruit Blast/Assets/_Project/Scripts/Features/Dimensions/Scale/ScaleProvider/ScaleProvider.cs
@@ -9,20 +9,54 @@ namespace _Project.Scripts.Features.Dimensions.Scale.ScaleProvider
     public class ScaleProvider : BaseFeature, IConfigurableFeature<ScaleProviderConfig>
     {
         [SerializeField] private ScaleProviderConfig _scaleProviderConfig;
+
+        private Vector2 _lastScale = Vector2.zero;
         
         public ScaleProviderConfig ScaleProviderConfig => _scaleProviderConfig;
         
         public float Scale { get; protected set; }
         public event Action<float> OnChangeScale;
 
+        private void FixedUpdate()
+        {
+            if (!IsResolutionChanged())
+            {
+                return;
+            }
+            
+            ChangeScale();
+        }
+        
         public void Configure(ScaleProviderConfig scaleProviderConfig)
         {
             _scaleProviderConfig = scaleProviderConfig;
         }
 
-        public void ChangeScale(Vector2 resolution)
+        public bool IsResolutionChanged()
         {
+            return !_lastScale.x.Equals(Screen.width) || !_lastScale.y.Equals(Screen.height);
+        }
+
+        public void ChangeScale()
+        {
+            _lastScale.x = Screen.width;
+            _lastScale.y = Screen.height;
+
+            var widthDelta = _scaleProviderConfig.BenchmarkResolution.x - _lastScale.x;
+            var heightDelta = _scaleProviderConfig.BenchmarkResolution.y - _lastScale.y;
+
+            if (widthDelta < heightDelta)
+            {
+                Scale = (_scaleProviderConfig.BenchmarkResolution.x - widthDelta) 
+                        / _scaleProviderConfig.BenchmarkResolution.x; 
+            }
+            else
+            {
+                Scale = (_scaleProviderConfig.BenchmarkResolution.y - heightDelta) 
+                        / _scaleProviderConfig.BenchmarkResolution.y;
+            }
             
+            OnChangeScale?.Invoke(Scale);
         }
     }
 }
