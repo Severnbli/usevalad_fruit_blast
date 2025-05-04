91e237b15dfb8f074a5a295647309e682e35da53
Usevalad Buben <usevaladbuben@gmail.com>
2025-05-04 23:05:59 +0300

    Complete scale feature

--- Usevalad Fruit Blast/Assets/_Project/Scripts/Features/Dimensions/Scale/ScaleProvider/ScaleProvider.cs
+++ Usevalad Fruit Blast/Assets/_Project/Scripts/Features/Dimensions/Scale/ScaleProvider/ScaleProvider.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using _Project.Scripts.Features.Common;
+using _Project.Scripts.Features.Field.FieldCatcher;
 using _Project.Scripts.Features.Field.FieldProvider;
 using _Project.Scripts.System;
+using _Project.Scripts.System.Logs.Logger;
 using UnityEngine;
 
 namespace _Project.Scripts.Features.Dimensions.Scale.ScaleProvider
@@ -9,22 +11,37 @@ namespace _Project.Scripts.Features.Dimensions.Scale.ScaleProvider
     public class ScaleProvider : BaseFeature, IConfigurableFeature<ScaleProviderConfig>
     {
         [SerializeField] private ScaleProviderConfig _scaleProviderConfig;
+        [SerializeField] private FieldCatcher _fieldCatcher;
 
-        private Vector2 _lastScale = Vector2.zero;
+        private Vector2 _lastSize = Vector2.zero;
         
         public ScaleProviderConfig ScaleProviderConfig => _scaleProviderConfig;
+        public FieldCatcher FieldCatcher => _fieldCatcher;
         
-        public float Scale { get; protected set; }
+        public float Scale { get; private set; }
         public event Action<float> OnChangeScale;
 
         private void FixedUpdate()
         {
-            if (!IsResolutionChanged())
+            var fieldCatcherSize = _fieldCatcher.GetCatcherSize();
+            
+            if (!IsSizeChanged(fieldCatcherSize))
             {
                 return;
             }
             
-            ChangeScale();
+            ChangeScale(fieldCatcherSize);
+        }
+
+        public override void Init()
+        {
+            base.Init();
+
+            if (!Context.TryGetComponentFromContainer(out _fieldCatcher))
+            {
+                LogManager.RegisterLogMessage(LogManager.LogType.Error, LogMessages.DependencyNotFound(
+                    GetType().ToString(), _fieldCatcher.GetType().ToString()));
+            }
         }
         
         public void Configure(ScaleProviderConfig scaleProviderConfig)
@@ -32,26 +49,25 @@ namespace _Project.Scripts.Features.Dimensions.Scale.ScaleProvider
             _scaleProviderConfig = scaleProviderConfig;
         }
 
-        public bool IsResolutionChanged()
+        public bool IsSizeChanged(Vector2 fieldCatcherSize)
         {
-            return !_lastScale.x.Equals(Screen.width) || !_lastScale.y.Equals(Screen.height);
+            return !_lastSize.x.Equals(fieldCatcherSize.x) || !_lastSize.y.Equals(fieldCatcherSize.y);
         }
 
-        public void ChangeScale()
+        public void ChangeScale(Vector2 fieldCatcherSize)
         {
-            _lastScale.x = Screen.width;
-            _lastScale.y = Screen.height;
+            _lastSize = fieldCatcherSize;
+            
+            var widthDelta = _scaleProviderConfig.BenchmarkSize.x - _lastSize.x;
+            var heightDelta = _scaleProviderConfig.BenchmarkSize.y - _lastSize.y;
 
-            var widthDelta = _scaleProviderConfig.BenchmarkResolution.x - _lastScale.x;
-            var heightDelta = _scaleProviderConfig.BenchmarkResolution.y - _lastScale.y;
-
-            if (widthDelta > heightDelta)
+            if (Mathf.Abs(widthDelta) > Mathf.Abs(heightDelta))
             {
-                Scale = _lastScale.x / _scaleProviderConfig.BenchmarkResolution.x;
+                Scale = _lastSize.x / _scaleProviderConfig.BenchmarkSize.x;
             }
             else
             {
-                Scale = _lastScale.y / _scaleProviderConfig.BenchmarkResolution.y;
+                Scale = _lastSize.y / _scaleProviderConfig.BenchmarkSize.y;
             }
             
             OnChangeScale?.Invoke(Scale);
