b65e2f61e0b6cd916f8f6331517eda1999a884ca
Usevalad Buben <usevaladbuben@gmail.com>
2025-05-04 22:35:36 +0300

    Add ObjectsContainerConfig

---
+++ Usevalad Fruit Blast/Assets/_Project/Scripts/Features/Lifecycle/Objects/ObjectsContainer/ObjectsContainerConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using _Project.Scripts.Features.Common;
+using UnityEngine;
+
+namespace _Project.Scripts.Features.Lifecycle.Objects.ObjectsContainer
+{
+    [Serializable]
+    public class ObjectsContainerConfig : IFeatureConfig
+    {
+        [SerializeField] private Transform _objectsContainerTransform;
+        
+        public Transform ObjectsContainerTransform => _objectsContainerTransform;
+    }
+}
