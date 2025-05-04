ac82bc5fa29a7e78cd07272537d25b1717d3ca60
Usevalad Buben <usevaladbuben@gmail.com>
2025-05-04 22:35:11 +0300

    Move ObjectsContainer to another catalog

---
+++ Usevalad Fruit Blast/Assets/_Project/Scripts/Features/Lifecycle/Objects/ObjectsContainer/ObjectsContainer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using _Project.Scripts.Features.Common;
+using UnityEngine;
+
+namespace _Project.Scripts.Features.Lifecycle.Objects.ObjectsContainer
+{
+    public class ObjectsContainer : BaseFeature, IConfigurableFeature<ObjectsContainerConfig>
+    {
+        [SerializeField] private ObjectsContainerConfig _objectsContainerConfig;
+        
+        public ObjectsContainerConfig ObjectsContainerConfig => _objectsContainerConfig;
+        public List<ContainerableObject> ContainerableObjects { get; protected set; } = new();
+
+        public void Configure(ObjectsContainerConfig objectsContainerConfig)
+        {
+            _objectsContainerConfig = objectsContainerConfig;
+        }
+        
+        public float GetTotalArea()
+        {
+            var totalArea = ContainerableObjects.Sum(x => x.GetArea());
+            
+            return totalArea;
+        }
+
+        public Transform GetObjectContainerTransform()
+        {
+            return _objectsContainerConfig.ObjectsContainerTransform;
+        }
+    }
+}
