e662b0dbb842508869ed9fee86ed1148076204ad
Usevalad Buben <usevaladbuben@gmail.com>
2025-05-04 22:39:30 +0300

    Update ObjectSpawner with ObjectsContainer

--- Usevalad Fruit Blast/Assets/_Project/Scripts/Features/Lifecycle/Spawners/ObjectSpawnerConfig.cs
+++ Usevalad Fruit Blast/Assets/_Project/Scripts/Features/Lifecycle/Spawners/ObjectSpawnerConfig.cs
@@ -8,13 +8,11 @@ namespace _Project.Scripts.Features.Lifecycle.Spawners
     public class ObjectSpawnerConfig : ScriptableObject, IFeatureConfig
     {
         [SerializeField] protected GameObject _prefab;
-        [SerializeField] protected Transform _objectContainerTransform;
         [SerializeField] protected List<SpawnGroupsConfig> _spawnGroups;
         [SerializeField] private float _minScale = 1f;
         [SerializeField] private float _maxScale = 1f;
         
         public GameObject Prefab => _prefab;
-        public Transform ObjectContainerTransform => _objectContainerTransform;
         public List<SpawnGroupsConfig> SpawnGroups => _spawnGroups;
         public float MinScale => _minScale;
         public float MaxScale => _maxScale;
